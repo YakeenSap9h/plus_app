1c2e5f709815bdec6c5f4a86971a54d0a160c18d
YakeenSap9h <yakeeny632@gmail.com>
2025-10-08 16:40:52 +0300

    fire base init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:plus_app/core/utils/app_router.dart';
+import 'package:plus_app/firebase_options.dart';
 
-void main() {
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const PulseApp());
 }
 
